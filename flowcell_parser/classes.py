92387b2101a2dd7819fbb2a06b2b56fc2466942b
Denis Moreno <denis.moreno@scilifelab.se>
2015-05-11 11:26:31 +0200

    fixed xml parsing for runParameters

--- flowcell_parser/classes.py
+++ flowcell_parser/classes.py
@@ -3,9 +3,31 @@ import os
 import csv
 import xml.etree.ElementTree as ET
 
-class XTenSampleSheetParser(object):
+class XTenDemultiplexingStatsParser(object):
     def __init__(self, path ):
+        if os.path.exists(path):
+            self.path=path
+            self.parse()
+        else:
+            raise os.error("XTen DemultiplexingStats.xml cannot be found at {0}".format(path))
 
+    def parse(self):
+        data={}
+        tree=ET.parse(self.path)
+        root = tree.getroot()
+        self.data=xml_to_dict(root)
+
+
+class XTenSampleSheetParser(object):
+    """Parses Xten Samplesheets, with their fake csv format.
+    Should be instancied with the samplesheet path as an argument.
+
+    .header : a dict containing the info located under the [Header] section
+    .settings : a dict containing the data from the [Settings] section
+    .reads : a list of the values in the [Reads] section
+    .data : a list of the values under the [Data] section. These values are stored in a dict format
+    .datafields : a list of field names for the data section"""
+    def __init__(self, path ):
         if os.path.exists(path):
             self.parse(path)
         else:
@@ -45,6 +67,7 @@ class XTenSampleSheetParser(object):
                     linedict[field]=row[field]
                 data.append(linedict)
 
+            self.datafields=reads.fieldnames
             self.data=data
             self.settings=settings
             self.header=header
@@ -52,6 +75,18 @@ class XTenSampleSheetParser(object):
 
 
 class XTenRunInfoParser(object):
+    """Parses Xten RunInfo.xml.
+    Should be instancied with the file path as an argument.
+
+    .data : a list of hand-picked values :
+     -Run ID
+     -Run Number
+     -Instrument
+     -Flowcell name
+     -Run Date
+     -Reads metadata
+     -Flowcell layout
+    """
     def __init__(self, path ):
         self.data={}
         self.path=path
@@ -81,6 +116,11 @@ class XTenRunInfoParser(object):
         
         
 class XTenRunParametersParser(object):
+    """Parses a runParameters.xml file.
+       This is a much more general xml parser, it will build a dict from the xml data.
+       Attributes might be replaced if children nodes have the same tag as the attributes
+       This does not happen in the current xml file, but if you're planning to reuse this, it may be of interest.
+    """
 
     def __init__(self, path ):
         self.data={}
@@ -94,39 +134,38 @@ class XTenRunParametersParser(object):
         data={}
         tree=ET.parse(self.path)
         root = tree.getroot()
-        self.data=self.xml_to_dict(root)
+        self.data=xml_to_dict(root)
         
 
-    def xml_to_dict(self,root):
-        current={}
-        if root.attrib:
-            current[root.tag]= root.attrib
+def xml_to_dict(root):
+    current=None
+    if root.attrib:
+        current= root.attrib
 
-        children=list(root)
-        if children:
-            duplicates={}
-            for child in children:
-                if len(root.findall(child.tag))>1:
-                    if child.tag not in duplicates:
-                        duplicates[child.tag]=[]
-                    lower=self.xml_to_dict(child)
-                    duplicates[child.tag].extend(lower.values())
-                    current.update({child.tag:duplicates})
-                else:
-                    lower=self.xml_to_dict(child)
-                    current.update(lower)
-        if root.text and root.text.strip() != "":
-            if root.tag in current:
-                if 'text' not in current[root.tag]:
-                    current[root.tag]['text']=root.text
-                else:
-                    #you're really pushing here, pal
-                    current[root.tag]['xml_text']=root.text
+    children=list(root)
+    if children:
+        current={}
+        duplicates={}
+        for child in children:
+            if len(root.findall(child.tag))>1:
+                if child.tag not in duplicates:
+                    duplicates[child.tag]=[]
+                lower=xml_to_dict(child)
+                duplicates[child.tag].extend(lower.values())
+                current.update(duplicates)
             else:
-                current[root.tag]=root.text
-        if not current:
-            current=None
-        return {root.tag:current}
+                lower=xml_to_dict(child)
+                current.update(lower)
+    if root.text and root.text.strip() != "":
+        if current:
+            if 'text' not in current:
+                current['text']=root.text
+            else:
+                #you're really pushing here, pal
+                current['xml_text']=root.text
+        else:
+            current=root.text
+    return {root.tag:current}
 
             
 
