e27f20e5f9a96239a3214f4acce5d584a76ee8a0
hiseq bioinfo <hiseq.bioinfo@preproc1.scilifelab.se>
2015-05-15 15:57:33 +0200

    script to parse and print FC status for reporting done

--- scripts/get_flowcell_stats.py
+++ scripts/get_flowcell_stats.py
@@ -2,16 +2,11 @@ import argparse
 import yaml
 import os
 import re
+import operator
 import flowcell_parser.classes as parser
-#from /home/hiseq.bioinfo/opt/flowcell_parser/flowcell_parser/classes.py import XTenRunParametersParser
 
 
 def flowcell_stats(flowcell):
-    import pdb
-    pdb.set_trace()
-    #demultiplexing_stats = parser.XTenDemultiplexingStatsParser("150508_ST-E00214_0034_BH3CYHCCXX/Demultiplexing/Stats/DemultiplexingStats.xml")    
-    #demultiplexing_stats.parse() #now I parsed the file
-    #LaneBarcodeParses = parser.XTenLaneBarcodeParser("150508_ST-E00214_0034_BH3CYHCCXX/Demultiplexing/)
     FC_re = r'(\d{6})_([ST-]*\w+\d+)_\d+_([AB]?)([A-Z0-9\-]+)'
     flowcell_dirname = os.path.basename(flowcell)
     m = re.match(FC_re, flowcell_dirname)
@@ -28,14 +23,42 @@ def flowcell_stats(flowcell):
             FC_dict[lane][sample] = {}   
         #now populate the hash table
         FC_dict[lane][sample]["Reads"]           = element["Clusters"]
-        FC_dict[lane][sample]["Yield (Mbases)"]   = element["Yield (Mbases)"]
-        if sample is not "unknown":
-            FC_dict[lane][sample]["% >= Q30bases"] = element["% >= Q30bases"]
-            
+        FC_dict[lane][sample]["Yield (Mbases)"]  = element["Yield (Mbases)"]
+        FC_dict[lane][sample]["% >= Q30bases"]   = element["% >= Q30bases"]
+    
+    lanes = []
+    for samples_lane in sorted(FC_dict.items(), key=operator.itemgetter(0)):
+        lane = { "lane": samples_lane[0], 
+                "Reads": 0 , 
+                "Yield (Mbases)": 0, 
+                "Raw Coverage":0 ,
+                "% >= Q30bases":0, 
+                "Undetemined Reads": 0, 
+                "% Undetermined Reads":0 }
+        #in case of demultiplexing the Q30 bases is belonging to one of the samples
+        for sample_name, sample in samples_lane[1].iteritems():
+            if  sample_name == "unknown":
+                lane["Undetemined Reads"] += int( re.sub(',', '', sample["Reads"])) 
+            else:
+                lane["Reads"]           += int( re.sub(',', '', sample["Reads"])) 
+                lane["Yield (Mbases)"]  += int( re.sub(',', '', sample["Yield (Mbases)"])) 
+                lane["% >= Q30bases"]   = sample["% >= Q30bases"]
+        lane["Raw Coverage"] = lane["Yield (Mbases)"]*1000000/3200000000
+        lane["% Undetermined Reads"]  = (float(lane["Undetemined Reads"])/(lane["Reads"]+lane["Undetemined Reads"]))*100
+        lanes.append(lane)
 
-    pdb.set_trace()
-    #/srv/illumina/HiSeq_X_data/150508_ST-E00214_0034_BH3CYHCCXX/Demultiplexing/Reports/html/H3CYHCCXX/all/all/all/laneBarcode.html
-    print "this is the end of the test\n"
+    print "FC\tLane\tReads\tYield(Mbases)\tRawCoverage\t%>Q30bases\tUndeteminedReads\t%Undetermined"
+    for lane in lanes:
+        import sys
+        sys.stdout.write('{}\t'.format(FC_id))
+        sys.stdout.write('{}\t'.format(lane["lane"]))
+        sys.stdout.write('{}\t'.format(lane["Reads"]))
+        sys.stdout.write('{}\t'.format(lane["Yield (Mbases)"]))
+        sys.stdout.write('{}\t'.format(lane["Raw Coverage"]))
+        sys.stdout.write('{}\t'.format(lane["% >= Q30bases"]))
+        sys.stdout.write('{}\t'.format(lane["Undetemined Reads"]))
+        sys.stdout.write('{}\n'.format(lane["% Undetermined Reads"]))
+
 
 
 
