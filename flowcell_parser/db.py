237d5defb29918a992464593c242a8fcef555fa6
Denis Moreno <denis.moreno@preproc2.scilifelab.se>
2015-05-28 13:45:15 +0200

    merging dicts instead of overwriting. tack @guillermo

--- flowcell_parser/db.py
+++ flowcell_parser/db.py
@@ -19,7 +19,8 @@ def update_doc(db, obj):
         doc_id = remote_doc.pop('_id')
         doc_rev = remote_doc.pop('_rev')
         if remote_doc != obj:
-            #if they are different, though they have the same name, upload the new one
+            #if they are different, merge the old into the new
+            obj=merge(obj, remote_doc)
             obj['_id'] = doc_id
             obj['_rev'] = doc_rev
             db[doc_id] = obj 
@@ -31,3 +32,22 @@ def update_doc(db, obj):
     else:
         log.warn("more than one row with name {0} found".format(obj['name']))
 
+#merges d2 in d1, keeps values from d1
+#taken from scilifelab
+def merge(d1, d2):
+    """ Will merge dictionary d2 into dictionary d1.
+    On the case of finding the same key, the one in d1 will be used.
+    :param d1: Dictionary object
+    :param s2: Dictionary object
+    """
+    for key in d2:
+        if key in d1:
+            if isinstance(d1[key], dict) and isinstance(d2[key], dict):
+                merge(d1[key], d2[key])
+            elif d1[key] == d2[key]:
+                pass # same leaf value
+            else:
+                log.debug("Values for key {key} in d1 and d2 differ, using d1's value".format(key=key))
+        else:
+            d1[key] = d2[key]
+    return d1
