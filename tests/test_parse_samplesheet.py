2a2c9b5322d1b4bde80a136ea837ab159f9698f9
Francesco Vezzi <francesco.vezzi@preproc2.scilifelab.se>
2015-09-16 09:17:19 +0200

    fixed unitests

--- tests/test_parse_samplesheet.py
+++ tests/test_parse_samplesheet.py
@@ -1,37 +1,45 @@
 
 import flowcell_parser.classes as classes
+import os
 
 
 def test_samplesheet():
-    k=classes.SampleSheetParser('../test_data/150424_ST-E00214_0031_BH2WY7CCXX/SampleSheet.csv')
+    path = os.path.dirname(os.path.abspath(__file__))
+    k=classes.SampleSheetParser(os.path.join(path, '../test_data/150424_ST-E00214_0031_BH2WY7CCXX/SampleSheet.csv'))
     assert(k.header is not None)
     assert(k.settings is not None)
     assert(k.reads is not None)
 
 def test_runinfo():
-    k=classes.RunInfoParser('../test_data/150424_ST-E00214_0031_BH2WY7CCXX/RunInfo.xml')
+    path = os.path.dirname(os.path.abspath(__file__))
+    k=classes.RunInfoParser(os.path.join(path, '../test_data/150424_ST-E00214_0031_BH2WY7CCXX/RunInfo.xml'))
     assert(k.data is not None)
 
 def test_runparameters():
-    k=classes.RunParametersParser('../test_data/150424_ST-E00214_0031_BH2WY7CCXX/runParameters.xml')
+    path = os.path.dirname(os.path.abspath(__file__))
+    k=classes.RunParametersParser(os.path.join(path, '../test_data/150424_ST-E00214_0031_BH2WY7CCXX/runParameters.xml'))
     assert(k.data is not None)
     
 
 def test_demuxstats():
-    k=classes.DemultiplexingStatsParser('../test_data/150424_ST-E00214_0031_BH2WY7CCXX/DemultiplexingStats.xml')
+    path = os.path.dirname(os.path.abspath(__file__))
+    k=classes.DemultiplexingStatsParser(os.path.join(path, '../test_data/150424_ST-E00214_0031_BH2WY7CCXX/DemultiplexingStats.xml'))
     assert(k.data is not None)
 
 def test_laneBarcode():
-    k=classes.LaneBarcodeParser('../test_data/150424_ST-E00214_0031_BH2WY7CCXX/Demultiplexing/Reports/html/H2WY7CCXX/all/all/all/laneBarcode.html')
+    path = os.path.dirname(os.path.abspath(__file__))
+    k=classes.LaneBarcodeParser(os.path.join(path, '../test_data/150424_ST-E00214_0031_BH2WY7CCXX/Demultiplexing/Reports/html/H2WY7CCXX/all/all/all/laneBarcode.html'))
     assert(k.flowcell_data is not None)
     assert(k.sample_data is not None)
 
 def test_lane():
-    k=classes.LaneBarcodeParser('../test_data/150424_ST-E00214_0031_BH2WY7CCXX/Demultiplexing/Reports/html/H2WY7CCXX/all/all/all/laneBarcode.html')
+    path = os.path.dirname(os.path.abspath(__file__))
+    k=classes.LaneBarcodeParser(os.path.join(path, '../test_data/150424_ST-E00214_0031_BH2WY7CCXX/Demultiplexing/Reports/html/H2WY7CCXX/all/all/all/laneBarcode.html'))
     assert(k.sample_data is not None)
 
 def test_parser():
-    k=classes.RunParser('../test_data/150424_ST-E00214_0031_BH2WY7CCXX')
+    path = os.path.dirname(os.path.abspath(__file__))
+    k=classes.RunParser(os.path.join(path, '../test_data/150424_ST-E00214_0031_BH2WY7CCXX'))
     assert(k.obj is not None)
     assert(k.obj is not {})
     assert(k.obj['run_setup'] is not None)
